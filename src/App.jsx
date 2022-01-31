75a82405422b69fb4cceab171b171c6613584318
Luispa23 <luis.pablo@bootcamp-upgrade.com>
2022-01-31 19:53:05 +0100

    IT Workgit add . but int´s showed in render

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,27 @@ import Axios from "axios";
 const App= ()=> {
   const [nba, setNba] = useState("");
   const [nbaChosen, setNbaChosen] = useState(false);
-  const [nbaName, setNbaName] = useState({
+  const [nbaName, setNbaName] = useState([{
     id:"",
     first_name:"",
     last_name:"",
     position: "",
     team: {},
 
-  });
+  }]);
 
   const searchNba =()=>{
 
     Axios.get(`https://www.balldontlie.io/api/v1/players/${nba}`).then(
       (res) =>{ console.log(res.data);
-         setNbaName({
+         setNbaName([{
            id:res.data.id,
            first_name:res.data.first_name,
            last_name:res.data.last_name,
            position:res.data.position,
            team:{ name:res.data.name},
       
-       });
+       }]);
       setNbaChosen(true);
     }
     )
@@ -48,7 +48,18 @@ const App= ()=> {
 
       </div>
       <div className='DisplaySection'>
-      {!nbaChosen ? (<h1>Please Choose a Nba Player</h1> ) : <h1>{nbaName}</h1>}
+     {!nbaChosen ? (
+          <h1> Please choose a player </h1>
+        ) : (
+          <>
+            <h1>Name:{nbaName.first_name}</h1>
+            {/* <img src={nbaName.image} alt={nbaName.name} /> */}
+            <h1>:Last Name {nbaName.last_name}</h1>
+            <h3>Position: {nbaName.position}</h3>
+            <h3>Team: {nbaName.team}</h3>
+            
+          </>
+        )}
       </div> 
     </div>
   );
